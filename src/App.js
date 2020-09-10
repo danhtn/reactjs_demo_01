f2531f80de1e10ad6cc1f04779a3e79ffc90533b
Danh Thanh Nguyen <danhtn@hitachiconsulting.com>
2020-09-11 00:10:16 +0700

    Add component Dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
 import Fallback from "./components/Fallback"
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Switch>
-            <Route path="/" exact component={SignIn} />
-            <Route path="/sign-up" exact component={SignUp} />
-            <Route path="*" component={Fallback} />
-          </Switch>
-        </Box>
-      </Container>
+      <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="*" component={Fallback} />
+      </Switch>
     </BrowserRouter>
   );
 }
