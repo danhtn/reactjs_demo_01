4cdd76b474525352ff7b65a817f35854f93d38c2
Danh Thanh Nguyen <danhtn@hitachiconsulting.com>
2020-09-10 23:01:31 +0700

    Add components: SignIn, SignUp, Fallback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Fallback from "./components/Fallback"
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Switch>
+            <Route path="/" exact component={SignIn} />
+            <Route path="/sign-up" exact component={SignUp} />
+            <Route path="*" component={Fallback} />
+          </Switch>
+        </Box>
+      </Container>
+    </BrowserRouter>
   );
 }
 
